f03626c9998b65d83a76e7dadc2173fe75055db1
Valeria Kolmykova <127939523+ssuccubuss@users.noreply.github.com>
2025-07-27 13:31:03 +0300

    Delete casetask3 directory

--- casetask3/includes/footer.php
+++
@@ -1,9 +0,0 @@
-        </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Мой блог. Все права защищены.</p>
-        </div>
-    </footer>
-    <script src="<?php echo BASE_URL; ?>js/script.js"></script>
-</body>
-</html>
--- casetask3/includes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-require_once 'auth.php';
-
-logoutUser();
-header("Location: " . BASE_URL . "login.php");
-exit();
-?>
